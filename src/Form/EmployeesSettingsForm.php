e40ab85758ed63e9f0bb34ef9a22cbb3d5e6f4b5
Pablo Villate <pablo.villate1@gmail.com>
2020-09-24 20:38:18 -0500

    ajuste instalación campos, creación-edición de nodos funcional

--- src/Form/EmployeesSettingsForm.php
+++ src/Form/EmployeesSettingsForm.php
@@ -4,6 +4,8 @@ namespace Drupal\employees\Form;
 
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
+use GuzzleHttp\Client;
+use Drupal\Component\Serialization\Json;
 
 class EmployeesSettingsForm extends ConfigFormBase {
     const SETTINGS = 'employees.settings';
@@ -38,6 +40,48 @@ class EmployeesSettingsForm extends ConfigFormBase {
         $this->configFactory->getEditable(static::SETTINGS)
             ->set('url_parques', $form_state->getValue('api_url'))
             ->save();
+
+        $employees = NULL;
+        $client = new Client();
+        $url = $form_state->getValue('api_url');
+
+        try {
+            $response = $client->get($url);
+
+            if ($response->getStatusCode() === 200) {
+                $json = $response->getBody()->getContents();
+                $employees = Json::decode($json);
+            } else {
+                throw New \RuntimeException(
+                    'Error webservice: ' . $response->getStatusCode()
+                );
+            }
+        } catch (\RuntimeException $e) {
+            watchdog_exception(__METHOD__, $e);
+        }
+
+        $batch = array(
+            'title' => t('Importando empleados...'),
+            'init_message' => t('Iniciando importación.'),
+            'progress_message' => t('Se han procesado @current de @total. Tiempo estimado: @estimate.'),
+            'error_message' => t('Error en la importación.'),
+            'finished' => '\Drupal\employees\ImportEmployees::importEmployeesFinishedCallback',
+        );
+        
+        //foreach($employees['data'] AS $employee) {
+            $batch['operations'] = array(
+                array(
+                    '\Drupal\employees\ImportEmployees::importEmployees',
+                    array($employees['data'], array())
+                ),
+            );
+        //}
+      
+        batch_set($batch);
+        \Drupal::messenger()->addMessage("Se importaron $total empleados");
+      
+        $form_state->setRebuild(TRUE);
+        
         return parent::submitForm($form, $form_state);
     }
 }
