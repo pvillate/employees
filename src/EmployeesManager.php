c27874d24ff4a99c27b1333b6266a84ccafa327a
Pablo Villate <pablo.villate1@gmail.com>
2020-09-26 14:12:00 -0500

    filtro por letras funcional sin ajax, TO-DO activar filtro sin necesidad de exponer manualmente otro filtro

--- src/EmployeesManager.php
+++ src/EmployeesManager.php
@@ -2,17 +2,36 @@
 
 namespace Drupal\employees;
 
-use Drupal\Core\Cache\CacheBackendInterface;
 use GuzzleHttp\Client;
+use Drupal\Component\Serialization;
 
 class EmployeesManager {
     
-    public function __construct() {
+    protected $httpClient;
+    protected $serialize;
 
+    public function __construct(Client $http_client, Serialization $serialize) {
+        $this->httpClient = $http_client;
+        $this->serialize = $serialize;
     }
 
     public function wsClient(string $url) {
+        $employees = NULL;
 
+        try {
+            $response = $this->httpClient->get($url);
+
+            if ($response->getStatusCode() === 200) {
+                $employees = $this->serialize->Json::decode($response);
+            } else {
+                throw New \RuntimeException(
+                    'Error webservice: ' . $response->getStatusCode()
+                );
+            }
+        } catch (\RuntimeException $e) {
+            watchdog_exception(__METHOD__, $e);
+        }
+
+        return $employees;
     }
-    
 }
